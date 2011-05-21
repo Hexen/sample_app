dc88d3113108bac75108f5398a1c41ab0e4472ce
Hexen <1483@bk.ru>
2011-05-21 21:33:26 +0400

    Done with user edit/update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,9 +21,23 @@ def sign_in(user)
     self.current_user = nil
   end
   
-  private
+   def current_user?(user)
+    user == current_user
+  end
 
-    def user_from_remember_token
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  
+   def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+  
+  private
+  
+      def user_from_remember_token
       User.authenticate_with_salt(*remember_token)
     end
 
@@ -31,4 +45,12 @@ def sign_in(user)
       cookies.signed[:remember_token] || [nil, nil]
     end
   
+   def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end
+    
 end
